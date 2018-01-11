c0312b13f8c409f01b35dcd3dab25da1dcf50a34
bigger <32177534+bigger404@users.noreply.github.com>
2018-01-11 09:27:36 -0800

    Update announce.py
    Added check for time of day, so it will only announce between 7am - 10pm.

--- announce.py
+++ announce.py
@@ -2,27 +2,28 @@
 from urllib.request import urlopen
 import subprocess
 import xml.etree.ElementTree as ET
+from datetime import datetime
 
 # Any string passed will pass to fesival for speech
 def say(phrase):
     subprocess.call('echo '+phrase+'|/usr/bin/festival --tts',shell=True)
 
-url = "http://www.hamqsl.com/solarxml.php"
-html = urlopen(url)
-xml=(bytes.decode(html.read()))
-root = ET.fromstring(xml)
-
-for band in root[0][16]:
-    print(band.attrib,band.text)
-    if(band.text=='Good'):
-        phrase = "Ham Radio Band. " + band.attrib['name'] + ". have good conditions"
-        phrase = phrase.replace('-',' and ')
-        phrase = phrase.replace('80m','80 meter ')
-        phrase = phrase.replace('40m','40 meter ')
-        phrase = phrase.replace('30m','30 meter ')
-        phrase = phrase.replace('20m','20 meter ')
-        phrase = phrase.replace('17m','17 meter ')
-        phrase = phrase.replace('12m','12 meter ')
-        phrase = phrase.replace('10m','10 meter ')
-        print(phrase)
-        say(phrase)
+if (datetime.now().hour >= 7 and datetime.now().hour <=20):
+    url = "http://www.hamqsl.com/solarxml.php"
+    html = urlopen(url)
+    xml=(bytes.decode(html.read()))
+    root = ET.fromstring(xml)
+    for band in root[0][16]:
+        print(band.attrib,band.text)
+        if(band.text=='Good'):
+            phrase = "Ham Radio Band. " + band.attrib['name'] + ". have good conditions"
+            phrase = phrase.replace('-',' and ')
+            phrase = phrase.replace('80m','80 meter ')
+            phrase = phrase.replace('40m','40 meter ')
+            phrase = phrase.replace('30m','30 meter ')
+            phrase = phrase.replace('20m','20 meter ')
+            phrase = phrase.replace('17m','17 meter ')
+            phrase = phrase.replace('12m','12 meter ')
+            phrase = phrase.replace('10m','10 meter ')
+            print(phrase)
+            say(phrase)
